00dd50e63b785638b3bb4f3ae59bb2b67063f883
Thomas Klemm <github@tklemm.eu>
2013-07-01 19:20:27 +0200

    Sign up, login, logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :confirmable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 end
